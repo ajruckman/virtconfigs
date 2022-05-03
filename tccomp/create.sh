df3b96bd5817d077a34421d8edbee6274e3eef72
A.J. Ruckman <aj@ruckman.dev>
2022-05-03 12:53:01 -0400

    Work

--- tccomp/create.sh
+++ tccomp/create.sh
@@ -3,12 +3,12 @@
 virsh pool-define    pool-pool0.xml
 virsh pool-autostart pool0
 
-virsh net-define    net-privnet0.xml
-virsh net-autostart privnet0
-
 virsh net-define    net-brnet0.xml
 virsh net-autostart brnet0
 
+virsh net-define    net-privnet0.xml
+virsh net-autostart privnet0
+
 virsh pool-start pool0
-virsh net-start  privnet0
 virsh net-start  brnet0
+virsh net-start  privnet0
